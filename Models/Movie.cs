4e4611f85b881d209e8f228d4512aa20a168db07
Curtis Godzisz <curtisgodzisz@gmail.com>
2020-08-21 16:49:33 -0400

    1st commit on movies

---
+++ Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        [Required]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Please select a number")]
+        public int ID { get; set; }
+
+        [Required]
+        [RegularExpression(@"[\Wa-zA-Z0-9!@#$%^&*()_+=\[{\]};:<>|./?,\\'""-]+", ErrorMessage = "Choose a valid movie title")]
+        [Range (1,50)]
+        public string Title { get; set; }
+
+        [Required]
+        [RegularExpression(@"[\Wa-zA-Z0-9!@#$%^&*()_+=\[{\]};:<>|./?,\\'""-]+", ErrorMessage = "Select a valid choice")]
+        public string Genre { get; set; }
+
+        [Required]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Choose a year between 1880 and the current year")]
+        
+        public int Year { get; set; }
+
+        //[Required]
+        //[RegularExpression(@".{0}[a-zA-Z0-9]+|.{1,50}[a-zA-Z0-9 ]")]
+        public List<string> Actors { get; set; }
+
+        //[Required]
+        //[RegularExpression(@"[a-zA-Z0-9]")]
+        public List<string> Directors { get; set; }
+    }
+}
